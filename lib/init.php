aa4b9617364ddba86d095afb5d3d7d3c886ed113
Jonathan Limbird <jonathan.limbird@gmail.com>
2014-09-27 09:45:05 -0500

    config changes

--- lib/init.php
+++ lib/init.php
@@ -41,8 +41,8 @@ function roots_widgets_init() {
     'id'            => 'sidebar-primary',
     'before_widget' => '<section class="widget %1$s %2$s">',
     'after_widget'  => '</section>',
-    'before_title'  => '<h3>',
-    'after_title'   => '</h3>',
+    'before_title'  => '<h5>',
+    'after_title'   => '</h5>',
   ));
 
   register_sidebar(array(
@@ -50,8 +50,8 @@ function roots_widgets_init() {
     'id'            => 'sidebar-footer',
     'before_widget' => '<section class="widget %1$s %2$s">',
     'after_widget'  => '</section>',
-    'before_title'  => '<h3>',
-    'after_title'   => '</h3>',
+    'before_title'  => '<h5>',
+    'after_title'   => '</h5>',
   ));
 }
 add_action('widgets_init', 'roots_widgets_init');
