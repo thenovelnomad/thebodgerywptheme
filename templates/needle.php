e3ea8d0821d6e54487c8986ce08ace92d36df108
Brad Orego <me+github@bradorego.com>
2014-09-27 17:51:40 -0500

    moar needle stuff

--- templates/needle.php
+++ templates/needle.php
@@ -1,9 +1,16 @@
 <script>
   $(function () {
-    var $needle = $('#needle');
+    var $needle = $('#needle'), needleTo = 0, rect = {}, $active = $('li.active a');
+    if ($active.length) {
+      rect = $active[0].getBoundingClientRect();
+      needleTo = rect.left + (rect.width / 2) - 12;
+    } else {
+      needleTo = document.body.getBoundingClientRect().width / 2;
+    }
+    $needle.css('left', needleTo + 'px');
     $('nav a').hover(function (e) {
-      var rect = this.getBoundingClientRect();
-      $needle.css('left', (rect.left + (rect.width / 2)) + 'px');
+      rect = this.getBoundingClientRect();
+      $needle.css('left', (rect.left + (rect.width / 2) - 12) + 'px');
     });
   });
 </script>
